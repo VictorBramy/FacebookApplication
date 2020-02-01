3c0c90f65f083f0446f71250789fe9f5006b5f44
VictorBramy <53178184+VictorBramy@users.noreply.github.com>
2020-02-01 02:38:04 +0200

    01/02/2020 changes

---
+++ FacebookApplication/CommandFolder/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacebookApplication.CommandFolder
+{
+    public class Menu : List<MenuItem>
+    {
+        public void StringAnalysisAndRunFunction(string i_Type)
+        {
+            foreach(MenuItem item in this)
+            {
+                if(item.Text == i_Type)
+                {
+                    item.CommandDeleage?.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ FacebookApplication/Panels/Developers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FacebookApplication
+{
+    public partial class Developers : UserControl
+    {
+        public Developers()
+        {
+            InitializeComponent();
+        }
+    }
+}
